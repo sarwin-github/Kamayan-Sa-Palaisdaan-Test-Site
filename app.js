7f5d864f42b21f7eaf1de3445d314016712e9e9f
Sherwin Dapito <dapito.sherwin@yahoo.com>
2017-03-21 14:06:00 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//include routes
+var index = require('./routes/index');
+
+var app = express();
+app.set('port', process.env.PORT || 3003);
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('images', __dirname + '/public/images');
+app.set('views', __dirname + '/views');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(__dirname));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/fonts/', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
+//app.use('/fonts/', express.static(__dirname + './font-awesome/fonts'));
+//app.use('/css/', express.static(__dirname + './font-awesome/css'));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
